33e26f6a817d65c902d2cb7b0e8413738abd10cb
Shahidavali Syed <syedshahidavali@gmail.com>
2022-12-01 13:25:02 +0530

    Updates

---
+++ precache-manifest.88efd8ad62375f05f33411f6dae218d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0cbaf75f579cda22af9",
+    "url": "/users/static/css/main.72d2382b.chunk.css"
+  },
+  {
+    "revision": "e0cbaf75f579cda22af9",
+    "url": "/users/static/js/main.e0cbaf75.chunk.js"
+  },
+  {
+    "revision": "faa1b14ccde4b34deff4",
+    "url": "/users/static/js/1.faa1b14c.chunk.js"
+  },
+  {
+    "revision": "64e8d901d91c90f998c2",
+    "url": "/users/static/js/runtime~main.64e8d901.js"
+  },
+  {
+    "revision": "4aa56f29b79961ef662b3428ddf0a56d",
+    "url": "/users/index.html"
+  }
+];
